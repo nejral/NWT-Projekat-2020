cd2b46161dd5312b0b3d3b4250662d9e504e8654
aminaanaan <amina-mo1@hotmail.com>
2020-05-03 01:42:36 +0200

    add special branch for systemevents microservice

---
+++ systemevents/src/main/java/com/system/systemevents/SystemeventsApplication.java
@@ -0,0 +1,13 @@
+package com.system.systemevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemeventsApplication.class, args);
+    }
+
+}
