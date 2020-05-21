ed483695b175c3bed99b4a97abeac3bd75c2e7e6
aminaanaan <amina-mo1@hotmail.com>
2020-05-21 12:26:20 +0200

    add rabbit consumer

--- HotelEna/src/main/java/com/example/hotel/ena/HotelEnaApplication.java
+++ HotelEna/src/main/java/com/example/hotel/ena/HotelEnaApplication.java
@@ -1,5 +1,10 @@
 package com.example.hotel.ena;
 
+import com.example.hotel.ena.rabbit.*;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.*;
+import org.springframework.amqp.rabbit.listener.*;
+import org.springframework.amqp.rabbit.listener.adapter.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -9,6 +14,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.*;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +26,39 @@ import java.util.List;
 @EnableFeignClients
 //@EnableAutoConfiguration
 public class HotelEnaApplication {
+    public static final String topicExchangeName = "spring-boot-exchange";
+
+    static final String queueName = "spring-boot";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HotelEnaApplication.class, args);
---
+++ HotelEna/src/main/java/com/example/hotel/ena/rabbit/Receiver.java
@@ -0,0 +1,21 @@
+package com.example.hotel.ena.rabbit;
+
+import org.springframework.stereotype.*;
+
+import java.util.concurrent.*;
+
+@Component
+public class Receiver {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+}
